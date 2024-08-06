49c70a8403bfd83a67bd6453098309c985680116
Amit <amit24ve@gmail.com>
2024-08-06 16:42:02 +0530

    Update

--- knn.py
+++ knn.py
@@ -8,18 +8,18 @@ from sklearn.metrics import classification_report,confusion_matrix
 
 data=pd.read_csv('Classified Data',index_col=0)
 df=pd.DataFrame(data)
-# print(df.info())
-# print(df.head())
+print(df.info())
+print(df.head())
 scaler=StandardScaler()
 scaler.fit(df.drop('TARGET CLASS',axis=1))
 scaled=scaler.transform(df.drop('TARGET CLASS',axis=1))
-# print(scaled)
+print(scaled)
 df_feat=pd.DataFrame(scaled,columns=df.columns[:-1])
-# print(df_feat.head())
+print(df_feat.head())
 X=df_feat
 y=df['TARGET CLASS']
 x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=101)
-# print(x_test)
+print(x_test)
 
 knn=KNeighborsClassifier(n_neighbors=1) #small datasets
 knn.fit(x_train,y_train)
